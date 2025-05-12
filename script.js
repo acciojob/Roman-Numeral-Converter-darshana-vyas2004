7e4f8df9008c9eef6c0c33b368d4db67e824a52c
darshana-vyas2004 <darshanavyas2021@gmail.com>
2025-05-12 12:57:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 function convertToRoman(num) {
-  if (num <= 0 || num > 100000) {
-    return "Invalid input";
-  }
-	 const romanMap = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
+  const romanMap = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1]
   ];
 
   let result = "";
@@ -29,9 +26,14 @@ function convertToRoman(num) {
 
   return result;
 }
-  //your code here
 
-}
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+
+// console.log(convertToRoman(36));
+
+
+// do not edit below this line
+module.exports = convertToRoman;
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
